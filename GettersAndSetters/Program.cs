de7ffc2608b7593e7f2571a70c3a03d1f54e5b86
Billy Dotson <billybad5@gmail.com>
2022-10-14 16:07:12 -0400

    Add project files.

---
+++ GettersAndSetters/Program.cs
@@ -0,0 +1,8 @@
+﻿using GettersAndSetters;
+
+Console.WriteLine("Getter and Setters");
+
+Movie avengers = new Movie("The Avengers", "Joss Whedon", "PG-13");
+Movie shrek = new Movie("Shrek", "Adam Adamson", "PG");
+shrek.Rating = "R";
+Console.WriteLine(shrek.Rating);
